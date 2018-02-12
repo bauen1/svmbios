fb3d0f6a5f477e282fbb3e220ef26baf1fd805c6
bauen1 <j2468h@gmail.com>
2018-02-12 13:32:16 +0100

    Added debugging information

--- src/entry-stub.s
+++ src/entry-stub.s
@@ -1,18 +1,29 @@
 [BITS 16]
 section main
-global _init
+global _init:function (_init.end - _init)
 _init:
 	cli
 	cld
 
 	; should always be mapped
 	mov esp, 0x7000
+	mov ax, 0x0
+	mov ss, ax
 
 	mov al, '!'
 	out 0xe9, al
 
-	xchg bx, bx
+	jmp _halt
+.end:
+
+global _halt:function (_halt.end - _halt)
+_halt:
+	cli
+.loop:
+	hlt
+	jmp .loop
+.end:
 
 section reset
-	jmp _init
+	jmp 0xf000:0x0
 align 16
