dd67ccfc711594bb1fe68735ac4f3c8bfc325b66
bauen1 <j2468h@gmail.com>
2018-02-12 13:39:00 +0100

    Added disable_nmi and enable_nmi

--- src/entry-stub.s
+++ src/entry-stub.s
@@ -10,6 +10,8 @@ _init:
 	mov ax, 0x0
 	mov ss, ax
 
+	call disable_nmi
+
 	mov al, '!'
 	call debug_putc
 
@@ -19,6 +21,21 @@ _init:
 global debug_putc:function (debug_putc.end - debug_putc)
 debug_putc:
 	out 0xe9, al
+	ret
+.end:
+
+global disable_nmi:function (disable_nmi.end - disable_nmi)
+disable_nmi:
+	in al, 0x70
+	or al, 0x80
+	out 0x70, al
+	ret
+.end:
+global enable_nmi:function (enable_nmi.end - enable_nmi)
+enable_nmi:
+	in al, 0x70
+	and al, 0x7F
+	out 0x70, al
 	ret
 .end:
 
