a1a29349847f919468ce45c31ed51fdea0bb5637
bauen1 <j2468h@gmail.com>
2018-02-14 11:17:02 +0100

    Fixed cpu.c

--- src/cpu.c
+++ src/cpu.c
@@ -22,15 +22,15 @@ uint32_t inl(uint16_t port) {
 }
 
 void outb(uint16_t port, uint8_t data) {
-	__asm__ __volatile__("outb %0, %1" : "=a" (data), "dN" (port));
+	__asm__ __volatile__("outb %0, %1" : : "=a" (data), "dN" (port));
 }
 
 void outw(uint16_t port, uint16_t data) {
-	__asm__ __volatile__("outw %0, %1" : "=a" (data), "dN" (port));
+	__asm__ __volatile__("outw %0, %1" : : "=a" (data), "dN" (port));
 }
 
 void outl(uint16_t port, uint32_t data) {
-	__asm__ __volatile__("outl %0, %1" : "=a" (data), "dN" (port));
+	__asm__ __volatile__("outl %0, %1" : : "=a" (data), "dN" (port));
 }
 
 __attribute__((noreturn))
